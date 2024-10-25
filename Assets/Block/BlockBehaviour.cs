436dd732337af8c015151735f1fe93dd5ed3854c
Khadijah Ameliyah (s5607568) <s5607568@bournemouth.ac.uk>
2024-10-25 18:56:07 +0100

    did stuff :)

--- Assets/Block/BlockBehaviour.cs
+++ Assets/Block/BlockBehaviour.cs
@@ -2,17 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public enum BlockType
+{
+    FORWARD,
+    BACK,
+    LEFT,
+    RIGHT,
+    JUMP,
+    THROW,
+    STRENGTH
+}
+
+
 public class BlockBehaviour : MonoBehaviour
 {
+
+    public BlockType blockType;
+    public Material material;
+
+
     // Start is called before the first frame update
     void Start()
     {
+
+        //refernce to material
+        material = GetComponent<Renderer>().material;
         
+        
+
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+
+
+
+
+
+
     }
+
+    
+  
+
+
+
+
+
+
+
+
 }
---
+++ Assets/Player/PlayerStuffDJ.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStuffDJ : MonoBehaviour
+{
+    public bool forward = false;
+    public bool back = false;
+    public bool right = false;
+    public bool left = false;
+    
+    
+    public bool chuck = false;
+    public bool jump = false;
+
+    public List<BlockBehaviour> blocks = new();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        foreach(BlockBehaviour block in blocks) 
+        {
+
+            if(block != null)
+            {
+                if(block.blockType == BlockType.FORWARD)
+                {
+                    forward = true;
+                }
+                if (block.blockType == BlockType.BACK)
+                {
+                    back = true;
+                }
+                if (block.blockType == BlockType.LEFT)
+                {
+                    left = true;
+                }
+                if (block.blockType == BlockType.RIGHT)
+                {
+                    forward = true;
+                }
+                if(block.blockType == BlockType.JUMP)
+                {
+
+                }
+
+            }
+
+
+
+
+        }
+
+
+
+
+
+    }
+}
