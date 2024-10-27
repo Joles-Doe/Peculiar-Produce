81dd8353273ff031baa8d0bb1099ba8319154884
Scott <scottlewwork@gmail.com>
2024-10-27 13:29:16 +0000

    Updated scripts

--- Assets/level/HazardCollider.cs
+++ Assets/level/HazardCollider.cs
@@ -6,7 +6,9 @@ public class HazardCollider : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
-        // Print the name of the object that collided with this object
-        Debug.Log("Collided with: " + collision.gameObject.name);
+        if (collision.gameObject.CompareTag("Player") == true)
+        {
+            collision.transform.root.GetComponentInChildren<PlayerControl>().damaged();
+        }
     }
 }
